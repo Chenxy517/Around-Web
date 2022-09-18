45cd1ca910ef5eae7dc484752d17338af8a8b514
chenxy <cxy20000517@gmail.com>
2022-09-17 21:19:53 -0400

    first commit

---
+++ backend/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://34.92.124.79:9200"
+	ES_USERNAME = "cxy20000517"
+	ES_PASSWORD = "Kevin517"
+	GCS_BUCKET  = "cxy20000517_bucket"
+)
